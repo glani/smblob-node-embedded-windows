1b16f1dd2146efd7a04f119b9ad977c160500ea5
denis <denis.yesakov@gmail.com>
2023-01-10 01:48:31 +0100

    skeleton linux example

--- embedded-windows/example/SMBlob_Node_Embedded_Windows_Example.cpp
+++ embedded-windows/example/SMBlob_Node_Embedded_Windows_Example.cpp
@@ -33,9 +33,9 @@ public:
 
     MyFrame(const wxString &title);
 
-    void OnButton1(wxCommandEvent &event);
+    void OnButtonInitialize(wxCommandEvent &);
 
-    void OnButton2(wxCommandEvent &event);
+    void OnButtonDestroy(wxCommandEvent &);
 
     void OnListBoxDoubleClick(wxCommandEvent &event);
 
@@ -49,19 +49,16 @@ DECLARE_EVENT_TABLE()
 
 // Declare some IDs. These are arbitrary.
 const int BOOKCTRL = 100;
-const int BUTTON1 = 101;
-const int BUTTON2 = 102;
-const int LISTBOX1 = 103;
-const int TEXTBOX1 = 104;
+const int BUTTON_INITIALIZE = 101;
+const int BUTTON_DESTROY = 102;
+const int TEXTBOX_LOG = 104;
 const int FILE_QUIT = wxID_EXIT;
 const int HELP_ABOUT = wxID_ABOUT;
 
 // Attach the event handlers. Put this after MyFrame declaration.
 BEGIN_EVENT_TABLE(MyFrame, wxFrame)
-                EVT_BUTTON(BUTTON1, MyFrame::OnButton1)
-                EVT_BUTTON(BUTTON2, MyFrame::OnButton2)
-                EVT_LISTBOX_DCLICK(LISTBOX1,
-                                   MyFrame::OnListBoxDoubleClick)
+                EVT_BUTTON(BUTTON_INITIALIZE, MyFrame::OnButtonInitialize)
+                EVT_BUTTON(BUTTON_DESTROY, MyFrame::OnButtonDestroy)
                 EVT_MENU(FILE_QUIT, MyFrame::OnQuit)
                 EVT_MENU(HELP_ABOUT, MyFrame::OnAbout)
 END_EVENT_TABLE()
@@ -80,9 +77,9 @@ MyFrame::MyFrame(const wxString &title)
     SetIcon(wxICON(example));
     wxMenu *fileMenu = new wxMenu;
     wxMenu *helpMenu = new wxMenu;
-    helpMenu->Append(HELP_ABOUT, _T("&About...\tF1"),
+    helpMenu->Append(HELP_ABOUT, _T("&About..."),
                      _T("Show about dialog"));
-    fileMenu->Append(FILE_QUIT, _T("E&xit\tAlt-X"),
+    fileMenu->Append(FILE_QUIT, _T("E&xit"),
                      _T("Quit this program"));
     wxMenuBar *menuBar = new wxMenuBar();
     menuBar->Append(fileMenu, _T("&File"));
@@ -92,39 +89,21 @@ MyFrame::MyFrame(const wxString &title)
     SetStatusText(_T("So far so good."), 0);
     SetStatusText(_T("Welcome."), 1);
 
-//    book = new wxBookCtrl(this, BOOKCTRL);
-//    wxPanel *panel = new wxPanel(book);
-//    new wxButton(panel, BUTTON1,
-//                 _T("Button &1"), wxPoint(50, 30), wxSize(100, 30));
-//    new wxButton(panel, BUTTON2,
-//                 _T("Button &2"), wxPoint(50, 80), wxSize(100, 30));
-//    book->AddPage(panel, _T("Tab1"), true);
-//
-//    wxString choices[] =
-//            {
-//                    _T("Washington"),
-//                    _T("Adams"),
-//                    _T("Jefferson"),
-//                    _T("Madison"),
-//                    _T("Lincoln"),
-//                    _T("One"),
-//                    _T("Two"),
-//                    _T("Three"),
-//                    _T("Four")
-//            };
-//    panel = new wxPanel(book);
-//    listbox1 = new wxListBox(panel, LISTBOX1,
-//                             wxPoint(0, 0), wxSize(150, 70),
-//                             9, choices, wxLB_SORT | wxLB_EXTENDED);
-//    book->AddPage(panel, _T("Tab2"), false);
-//
-//    panel = new wxPanel(book);
-//    wxBoxSizer *mysizer = new wxBoxSizer(wxVERTICAL);
-//    panel->SetSizer(mysizer);
-//    textLog = new wxTextCtrl(panel, TEXTBOX1, _T("Log\n"),
-//                             wxPoint(0, 250), wxSize(100, 50), wxTE_MULTILINE);
-//    mysizer->Add(textLog, 1, wxEXPAND | wxALL, 5);
-//    book->AddPage(panel, _T("Debug"), false);
+    auto book = new wxBookCtrl(this, BOOKCTRL);
+    wxPanel *panel = new wxPanel(book);
+    new wxButton(panel, BUTTON_INITIALIZE,
+                 _T("&Initialize"), wxPoint(50, 30), wxSize(100, 30));
+    new wxButton(panel, BUTTON_DESTROY,
+                 _T("&Destroy"), wxPoint(50, 80), wxSize(100, 30));
+    book->AddPage(panel, _T("Initialize Embedded Application"), true);
+
+    panel = new wxPanel(book);
+    wxBoxSizer *mysizer = new wxBoxSizer(wxVERTICAL);
+    panel->SetSizer(mysizer);
+    textLog = new wxTextCtrl(panel, TEXTBOX_LOG, _T("Log\n"),
+                             wxPoint(0, 250), wxSize(100, 50), wxTE_MULTILINE);
+    mysizer->Add(textLog, 1, wxEXPAND | wxALL, 5);
+    book->AddPage(panel, _T("Debug"), false);
 }
 
 void MyFrame::OnQuit(wxCommandEvent & WXUNUSED(event)) {
@@ -140,12 +119,12 @@ void MyFrame::OnAbout(wxCommandEvent & WXUNUSED(event)) {
                  wxOK | wxICON_INFORMATION, this);
 }
 
-void MyFrame::OnButton1(wxCommandEvent & WXUNUSED(event)) {
+void MyFrame::OnButtonInitialize(wxCommandEvent & WXUNUSED(event)) {
     wxMessageBox("Click1", "Click",
                  wxOK | wxICON_INFORMATION, this);
 }
 
-void MyFrame::OnButton2(wxCommandEvent & WXUNUSED(event)) {
+void MyFrame::OnButtonDestroy(wxCommandEvent & WXUNUSED(event)) {
     wxMessageBox("Click2", "Click",
                  wxOK | wxICON_INFORMATION, this);
 }
